7a231dec1abc0db73b6200867a31f396bfea2357
Keith Bennett <keith@airmap.com>
2022-01-24 18:19:27 -0600

    shellcheck

--- dockeragent/start.bash
+++ dockeragent/start.bash
@@ -32,7 +32,7 @@ cleanup() {
 		# If the agent has some running jobs, the configuration removal process will fail.
 		# So, give it some time to finish the job.
 		while true; do
-			./config.sh remove --unattended --auth PAT --token $(cat "${AZP_TOKEN_FILE}") && break
+			./config.sh remove --unattended --auth PAT --token "$(cat "${AZP_TOKEN_FILE}")" && break
 			"Retrying in 30 seconds..."
 			sleep 30
 		done
@@ -51,14 +51,14 @@ export VSO_AGENT_IGNORE=AZP_TOKEN,AZP_TOKEN_FILE
 print_header "1. Determining matching Azure Pipelines agent..."
 
 AZP_AGENT_PACKAGES=$(curl -LsS \
-	-u user:$(cat "${AZP_TOKEN_FILE}") \
+	-u "user:$(cat "${AZP_TOKEN_FILE}")" \
 	-H 'Accept:application/json;' \
-	"${AZP_URL}/_apis/distributedtask/packages/agent?platform=$TARGETARCH&top=1"
+	"${AZP_URL}/_apis/distributedtask/packages/agent?platform=${TARGETARCH}&top=1"
 )
 
 AZP_AGENT_PACKAGE_LATEST_URL=$(echo "${AZP_AGENT_PACKAGES}" | jq -r '.value[0].downloadUrl')
 
-if [ -z "${AZP_AGENT_PACKAGE_LATEST_URL}" -o "${AZP_AGENT_PACKAGE_LATEST_URL}" == "null" ]; then
+if [ -z "${AZP_AGENT_PACKAGE_LATEST_URL}" ] || [ "${AZP_AGENT_PACKAGE_LATEST_URL}" == "null" ]; then
 	>&2 echo "error: could not determine a matching Azure Pipelines agent"
 	>&2 echo "check that account '$AZP_URL' is correct and the token is valid for that account"
 	exit 1
@@ -68,6 +68,8 @@ print_header "2. Downloading and extracting Azure Pipelines agent..."
 
 curl -LsS "${AZP_AGENT_PACKAGE_LATEST_URL}" | tar -xz
 
+# env.sh comes from the azure agent installer. it can't be shellchecked from here
+# shellcheck disable=SC1091
 source ./env.sh
 
 print_header "3. Configuring Azure Pipelines agent..."
@@ -76,7 +78,7 @@ print_header "3. Configuring Azure Pipelines agent..."
 	--agent "${AZP_AGENT_NAME:-$(hostname)}" \
 	--url "${AZP_URL}" \
 	--auth PAT \
-	--token $(cat "${AZP_TOKEN_FILE}") \
+	--token "$(cat "${AZP_TOKEN_FILE}")" \
 	--pool "${AZP_POOL:-Default}" \
 	--work "${AZP_WORK:-_work}" \
 	--replace \
